2292379ca93f6586827181162f215f7bbd0bbef0
Anesu <106879570+Anesu-R@users.noreply.github.com>
2022-08-19 16:05:40 +0100

    All files added
    Key files: index.html; page2.html; page3.html; style.css; app.js; coral-reef3.jpg; cape-penguin.jpg; hectors-dolphin.jpg

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Would you like to sign up to our newsletter?");
+var answer = prompt("Yes [y] or No [n]");
+
+if (answer === 'y') {
+    prompt("Enter email");
+} else {
+    alert("Thanks for visiting!")
+};
